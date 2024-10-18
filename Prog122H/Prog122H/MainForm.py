af66c8101fe26d9d89d1d4fdb9b3149bb2d6a175
carterc2209 <campbell.c@stu.janesville.k12.wi.us>
2024-10-18 14:30:18 -0500

    Prog122B-H 10/18

---
+++ Prog122H/Prog122H/MainForm.py
@@ -0,0 +1,92 @@
+﻿import math
+import System.Drawing
+import System.Windows.Forms
+
+from math import *
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+    def __init__(self):
+        self.InitializeComponent()
+    
+    def InitializeComponent(self):
+        self._listBox1 = System.Windows.Forms.ListBox()
+        self._button1 = System.Windows.Forms.Button()
+        self._button2 = System.Windows.Forms.Button()
+        self._button3 = System.Windows.Forms.Button()
+        self.SuspendLayout()
+        # 
+        # listBox1
+        # 
+        self._listBox1.FormattingEnabled = True
+        self._listBox1.Location = System.Drawing.Point(12, 12)
+        self._listBox1.Name = "listBox1"
+        self._listBox1.Size = System.Drawing.Size(523, 290)
+        self._listBox1.TabIndex = 0
+        # 
+        # button1
+        # 
+        self._button1.BackColor = System.Drawing.Color.FromArgb(255, 128, 128)
+        self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 20.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button1.Location = System.Drawing.Point(12, 349)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(159, 81)
+        self._button1.TabIndex = 1
+        self._button1.Text = "Calculate"
+        self._button1.UseVisualStyleBackColor = False
+        self._button1.Click += self.Button1Click
+        # 
+        # button2
+        # 
+        self._button2.BackColor = System.Drawing.Color.FromArgb(255, 128, 128)
+        self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 20.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button2.Location = System.Drawing.Point(177, 349)
+        self._button2.Name = "button2"
+        self._button2.Size = System.Drawing.Size(193, 81)
+        self._button2.TabIndex = 2
+        self._button2.Text = "Clear"
+        self._button2.UseVisualStyleBackColor = False
+        self._button2.Click += self.Button2Click
+        # 
+        # button3
+        # 
+        self._button3.BackColor = System.Drawing.Color.FromArgb(255, 128, 128)
+        self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 20.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button3.Location = System.Drawing.Point(376, 349)
+        self._button3.Name = "button3"
+        self._button3.Size = System.Drawing.Size(159, 81)
+        self._button3.TabIndex = 3
+        self._button3.Text = "Exit"
+        self._button3.UseVisualStyleBackColor = False
+        self._button3.Click += self.Button3Click
+        # 
+        # MainForm
+        # 
+        self.BackColor = System.Drawing.Color.FromArgb(128, 128, 255)
+        self.ClientSize = System.Drawing.Size(547, 442)
+        self.Controls.Add(self._button3)
+        self.Controls.Add(self._button2)
+        self.Controls.Add(self._button1)
+        self.Controls.Add(self._listBox1)
+        self.Name = "MainForm"
+        self.Text = "Prog122H"
+        self.ResumeLayout(False)
+
+
+    def Button3Click(self, sender, e):
+        Application.Exit()
+
+    def Button2Click(self, sender, e):
+        self._listBox1.Items.Clear()
+
+    def Button1Click(self, sender, e):
+        heading = "Number\t\tSquare\t\tSquare Root\tCube\t\tFourth Root"
+        self._listBox1.Items.Add(heading)
+        for num in range(1, 21):
+            nsqrd = num**2
+            nsqrt = sqrt(num)
+            ncube = num**3
+            nfrth = num **(1.0/4)
+            line = str(num) + "\t\t" + str(nsqrd) + "\t\t" + str(round(nsqrt, 4)) + "\t\t" + str(ncube) + "\t\t" + str(round(nfrth, 4))
+            self._listBox1.Items.Add(line)
